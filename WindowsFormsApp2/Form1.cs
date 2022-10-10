1297cb3f4dd5a609de3fe5ddc28e073e57b29f3c
PhoeniXfire208 <subi9999@mail.ru>
2022-10-10 21:28:26 +0800

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox2.Text = Properties.Settings.Default.result.ToString();
+            textBox1.Text = Properties.Settings.Default.str.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var str = textBox1.Text;
+            var result = Logic.Compare(str);
+            this.textBox2.Text = result;
+
+            Properties.Settings.Default.str = str;
+            Properties.Settings.Default.result = result;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                button1_Click(button1, null);
+            }
+        }
+
+        private void заданиеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Развернуть все слова в предложении");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
---
+++ WindowsFormsApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    
+    public class Logic
+    {
+        public static string Revers(string str)
+        {
+            string str2;
+            string[] mas = str.Split(' ');
+            
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = new string(mas[i].Reverse().ToArray());
+            }
+
+            str2 = string.Join(" ", mas);
+
+            return str2;
+        }
+
+        public static string Compare(string str)
+        {
+            var A = Logic.Revers(str);
+            string outMessage = A;
+
+            return outMessage;
+        }
+    }
+}
